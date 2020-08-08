d29a142d309f3398600d268cdac8be63edf08d87
Miloni Mittal <milonimittal2015@gmail.com>
2020-08-09 00:08:57 +0530

    added files

---
+++ main.py
@@ -0,0 +1,15 @@
+from InstaBot import InstaBot
+
+username=input("Enter your username: ")
+password=input("Enter your password: ")
+choice_str=input("Input 1 for account scraping, 2 for hashtag scraping: ")
+choice=int(choice_str)
+
+if choice==1:
+	target_account=input("Enter name of the account you want to scrape: ")
+	myBot=InstaBot(username, password)
+	myBot.scrape_account(target_account)
+elif choice==2:
+	target_hashtag=input("Enter the hashtag you want to scrape: ")
+	myBot=InstaBot(username, password)
+	myBot.scrape_hashtag(target_hashtag)
